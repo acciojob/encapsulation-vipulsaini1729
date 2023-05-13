5d790a58783558f9491b138d8dbc1bb34ce05168
Vipul Saini <111876989+vipulsaini1729@users.noreply.github.com>
2023-05-14 01:37:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+  public main(){
+  }
+  public static void main(String [] args){
+    Product p  = new Product();
+    p.product(1,2);
+    p.product(1,2,3);
+    p.product(1.0,2.0);
+  }
+  public static class Product{
+   public int product(int a, int b){
+    return a*b; 
+   }
+    public int product(int a, int b, int c){
+    return a*b*c; 
+   }
+    public double product(double a, double b){
+    return a*b; 
+   }
+  }
   
 }
